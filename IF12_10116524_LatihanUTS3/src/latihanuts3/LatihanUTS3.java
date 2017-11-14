ceec890b7b8bc24e12aebd990ba3385c5d8d3117
sissyoctavia <32486234+sissyoctavia@users.noreply.github.com>
2017-11-15 00:05:34 +0700

    Add files via upload

---
+++ IF12_10116524_LatihanUTS3/src/latihanuts3/LatihanUTS3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanuts3;
+
+/**
+ *
+ * @author SISSY OCTAVIA SHELYNA (10116524) PBO-12
+ */
+public class LatihanUTS3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
